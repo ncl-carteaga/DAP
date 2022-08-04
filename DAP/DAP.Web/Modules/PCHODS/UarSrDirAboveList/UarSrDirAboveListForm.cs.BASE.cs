332b7b8d611189ba7e58357650f383f309ce9c27
jugomez <jugomez@nclcorp.com>
2022-08-04 12:12:18 -0400

    Conflicts

---
+++ DAP/DAP.Web/Modules/PCHODS/UarSrDirAboveList/UarSrDirAboveListForm.cs.BASE.cs
@@ -0,0 +1,22 @@
+﻿
+namespace DAP.PCHODS.Forms
+{
+    using Serenity;
+    using Serenity.ComponentModel;
+    using Serenity.Data;
+    using System;
+    using System.ComponentModel;
+    using System.Collections.Generic;
+    using System.IO;
+
+    [FormScript("PCHODS.UarSrDirAboveList")]
+    [BasedOnRow(typeof(Entities.UarSrDirAboveListRow), CheckNames = true)]
+    public class UarSrDirAboveListForm
+    {
+        public String Jobtitle { get; set; }
+        public Int32 IsSrDirAbove { get; set; }
+        public DateTime DateEffective { get; set; }
+        public DateTime DateExpires { get; set; }
+        public String RowChangeReason { get; set; }
+    }
+}
