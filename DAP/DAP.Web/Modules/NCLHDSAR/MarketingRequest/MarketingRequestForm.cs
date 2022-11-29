6f716c51ee92310186b98c2420cd9ba57019b2a7
carteaga <carteaga@nclcorp.com>
2022-11-29 10:49:06 -0500

    Update to dialog validation, form layout.

--- DAP/DAP.Web/Modules/NCLHDSAR/MarketingRequest/MarketingRequestForm.cs
+++ DAP/DAP.Web/Modules/NCLHDSAR/MarketingRequest/MarketingRequestForm.cs
@@ -16,25 +16,41 @@ namespace DAP.NCLHDSAR.Forms
         [Category("General")]
          public Int16 RequestTypeId { get; set; }
          public Int16 SourceId { get; set; }
+        [HalfWidth]
         public Int16 BrandId { get; set; }
+        [AlignRight, HalfWidth]
         public String ContactId { get; set; }
+        [HalfWidth]
         public String FirstName { get; set; }
+        [AlignRight, HalfWidth]
         public String LastName { get; set; }
+        [HalfWidth]
         public String Phone { get; set; }
+        [AlignRight, HalfWidth]
         public String EmailAddress { get; set; }
+
         [Category("Opt Out")]
+        [HalfWidth]
         public Int16 OptOutStatusID { get; set; }
+        [AlignRight, HalfWidth]
         public Int16 ChannelId { get; set; }
+
+        [Category("Return Mail")]
+        public Boolean IsReturnedMailCd { get; set; }
+
         [Category("Change of Address")]
         public String Title { get; set; }
         public String Address1 { get; set; }
         public String Address2 { get; set; }
+        [HalfWidth]
         public String City { get; set; }
+        [AlignRight, HalfWidth]
         public String State { get; set; }
+        [HalfWidth]
         public String Zip { get; set; }
+        [AlignRight, HalfWidth]
         public String Country { get; set; }
-        [Category("Return Mail")]
-        public Boolean IsReturnedMailCd { get; set; }
+
         [Visible(false)]
         public String CreatedBy { get; set; }
         [Visible(false)]
