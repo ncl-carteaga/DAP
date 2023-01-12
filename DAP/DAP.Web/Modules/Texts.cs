df8e6977431b1ca7e93895e1312a001bfa9f3ac9
jugomez <jugomez@nclcorp.com>
2023-01-12 13:25:54 -0500

    Published 1.61

--- DAP/DAP.Web/Modules/Texts.cs
+++ DAP/DAP.Web/Modules/Texts.cs
@@ -253,7 +253,7 @@ namespace DAP
 
             public static class Layout
             {
-                public static LocalText FooterCopyright = "Copyright (c) 2023 v1.60";
+                public static LocalText FooterCopyright = "Copyright (c) 2023 v1.61";
                 public static LocalText FooterInfo = "Serenity Platform";
                 public static LocalText FooterRights = "All rights reserved.";
                 public static LocalText GeneralSettings = "General Settings";
