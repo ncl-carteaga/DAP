e2a00f4dfff2e3de5cd634dfade41479bd21bb62
jugomez <jugomez@nclcorp.com>
2023-01-12 10:18:13 -0500

    local

--- DAP/DAP.Web/Scripts/site/DAP.Web.js
+++ DAP/DAP.Web/Scripts/site/DAP.Web.js
@@ -11664,6 +11664,7 @@ var DAP;
                 dialog.dialogOpen();
             };
             SailingMasterSuppGrid.prototype.getQuickFilters = function () {
+                var _this = this;
                 // get quick filter list from base class
                 var filters = _super.prototype.getQuickFilters.call(this);
                 // get filter for SailDat field and set default value
@@ -11672,6 +11673,7 @@ var DAP;
                     var date = new Date();
                     var t = Q.formatDate(date.toDateString(), "MM/dd/yyyy");
                     w.value = t;
+                    _this.refresh();
                 };
                 return filters;
             };
