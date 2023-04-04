cbd6886771fe2d99db6692145254901e1999e96f
carteaga <carteaga@nclcorp.com>
2023-04-04 16:36:41 -0400

    Added repo validation

--- DAP/DAP.Web/Modules/DWSupport/CmiSupp/CmiSuppRepository.cs
+++ DAP/DAP.Web/Modules/DWSupport/CmiSupp/CmiSuppRepository.cs
@@ -1,6 +1,7 @@
 ﻿
 namespace DAP.DWSupport.Repositories
 {
+    using DAP.DWSupport.Entities;
     using Serenity;
     using Serenity.Data;
     using Serenity.Services;
@@ -37,7 +38,23 @@ namespace DAP.DWSupport.Repositories
             return new MyListHandler().Process(connection, request);
         }
 
-        private class MySaveHandler : SaveRequestHandler<MyRow> {
+        private class MySaveHandler : SaveRequestHandler<MyRow>
+        {
+            protected override void BeforeSave()
+            {
+                base.BeforeSave();    
+
+                if (IsUpdate || IsCreate)
+                {
+                    var sDate = getStartDate();
+                    var eDate = getEndDate();
+
+                    if (!ValidStartEndDates())
+                        throw new ValidationError("Start and end dates must match voyage code date. \nStart: "+sDate+"\nEnd: "+eDate);
+                }
+            }
+
+
             protected override void SetInternalFields()
             {
                 base.SetInternalFields();
@@ -55,6 +72,48 @@ namespace DAP.DWSupport.Repositories
                     Row.ModifiedByNam = user.DisplayName.ToUpper();
                     Row.ModifiedTs = DateTime.Now;
                 }
+            }
+
+
+            private bool ValidStartEndDates()
+            {
+                // compare parsed dates to entered dates
+                var sDate = getStartDate();
+                var eDate = getEndDate();
+                var t1 = Row.VoyageStartDate.Value.CompareTo(sDate);
+                var t2 = Row.VoyageEndDate.Value.CompareTo(eDate);
+                var sum = t1 + t2;  // both comparison must equal 0
+
+                // validate start date parsed correctly
+                if (sDate == DateTime.MinValue) return false;
+
+                return (sum == 0);
+            }
+
+            private DateTime getStartDate()
+            {
+                // parse start cruise date     e.g:     PRL-20160111-04-MIA-MIA     /yyyy-mm-dd
+                DateTime sDate;
+                var str = Row.VoyageCd.Substring(4, 8).Insert(4, "-").Insert(7, "-"); // transform date string: 20160111 > 2016-01-11
+                var validDate = DateTime.TryParse(str, out sDate);
+
+                // invalid date parsing, return default value
+                if (!validDate) sDate = DateTime.MinValue;
+
+                return sDate;
+            }
+
+            private DateTime getEndDate()
+            {
+                var dDate = DateTime.MinValue;
+                if (getStartDate() == dDate) return dDate;
+
+                DateTime eDate;
+                var length = Convert.ToDouble(Row.VoyageCd.Substring(13, 2));
+                // get end date by adding trip length to start date 
+                eDate = getStartDate().AddDays(length);
+
+                return eDate;
             }
         }
         private class MyDeleteHandler : DeleteRequestHandler<MyRow> { }
