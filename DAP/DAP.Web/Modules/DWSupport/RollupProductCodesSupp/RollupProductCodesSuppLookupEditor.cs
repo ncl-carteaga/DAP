8f0c75a4dde7ac9ea9ea3c30515b740a41bf25df
carteaga <carteaga@nclcorp.com>
2023-04-27 09:06:56 -0400

    Code lookup

---
+++ DAP/DAP.Web/Modules/DWSupport/RollupProductCodesSupp/RollupProductCodesSuppLookupEditor.cs
@@ -0,0 +1,21 @@
+﻿namespace DAP.DWSupport.Lookups
+{
+    using Entities;
+    using Serenity.ComponentModel;
+    using Serenity.Data;
+    using Serenity.Web;
+
+    //  =============== [MODULE FOR SHIP CODE LOOKUPS] =============== //
+    //      ShipRow can be used as a lookup script. However,
+    //      this module will filter the codes by specific Brand.
+    //  =============== ============================ =============== //
+
+    [LookupScript]
+    public class RollupProductCodesSuppLookupEditor : RowLookupScript<Entities.ProductCodeSuppRow>
+    {
+        public RollupProductCodesSuppLookupEditor()
+        {
+            IdField = TextField = ProductCodeSuppRow.Fields.ProductCd.PropertyName;
+        }
+    }
+}
