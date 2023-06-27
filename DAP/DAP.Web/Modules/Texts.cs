0a3888b8107aba17810a371a46e3aa0da46ec10f
carteaga <carteaga@nclcorp.com>
2023-06-27 10:04:41 -0400

    New version published to DEV

--- DAP/DAP.Web/Modules/Texts.cs
+++ DAP/DAP.Web/Modules/Texts.cs
@@ -253,7 +253,7 @@ namespace DAP
 
             public static class Layout
             {
-                public static LocalText FooterCopyright = "Copyright (c) 2023 v1.73";
+                public static LocalText FooterCopyright = "Copyright (c) 2023 v1.74";
                 public static LocalText FooterInfo = "Serenity Platform";
                 public static LocalText FooterRights = "All rights reserved.";
                 public static LocalText GeneralSettings = "General Settings";
